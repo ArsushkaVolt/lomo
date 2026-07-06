8e8c67410da7ad4629d2400e1d6aeb732b70facd
Arsenii Turikov <150505627+ArsushkaVolt@users.noreply.github.com>
2026-07-06 23:38:31 +0300

    Основа интерфейса

---
+++ UIM/WpfApp1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public enum UserRole
+    {
+        Operator,
+        Metrologist,
+        Administrator
+    }
+
+    public class User
+    {
+        public string FullName { get; set; }
+        public UserRole Role { get; set; }
+        public string Login { get; set; }
+        // Другие метрологические данные...
+    }
+}
