9478eb164c06676e759774bcf69c370af0dcd1f4
ShchcerbakoVV <ilya.shcherbakov.05@list.ru>
2026-07-11 21:14:31 +0300

    Соединил math c main => main ветка работает (тут нет авторизации арса)

--- UIM/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ UIM/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a4e0ea7cb62746df9ed4318beb68d35bfbaceb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c451d5fb61ca5fe5d47ce5f7716222be0ecccb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
